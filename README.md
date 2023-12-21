c508b57e2ed1d8b3168e79f3e052786b766a78c4
stsaria <solothunder.autoer@gmail.com>
2023-12-21 11:18:47 +0900

    tast

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 AllServer is a distributed, low-cost minecraft server delivery system.
 Please select readme language.
 ## Language
-[Japanise(日本語)](README.ja.md)
+[Japanese(日本語)](README.ja.md)
--- src/install_auto_run.py
+++ src/install_auto_run.py
@@ -13,13 +13,14 @@ def register_auto_run(start_program : str, argv : str):
             start_command = f"python -u {start_program} "+argv
     else:
         start_command = absolute_path+"/"+os.path.basename(start_program.replace("./", ""))+" "+argv
+    argv = argv.replace("--start-", "")
     if user_use_platform == "Linux":
         if not shutil.which("systemctl"):
             print(lang["Message"]["InstallAutoRun"]["Message"][1])
             print(lang["Message"]["InstallAutoRun"]["Message"][3])
             return 1
         try:
-            with open("/etc/systemd/system/allserver-"+argv.replace("--", "")+".service", encoding="utf-8", mode="w") as f:
+            with open("/etc/systemd/system/allserver-"+argv+".service", encoding="utf-8", mode="w") as f:
                 f.write(f"""[Unit]
 Description=AllServer Systemd
 After=network.target
@@ -29,14 +30,14 @@ Restart=always
 ExecStart={start_command}
 [Install]
 WantedBy=multi-user.target""")
-            subprocess.run("systemctl daemon-reload && systemctl enable allserver-"+argv.replace("--", "")+".service && systemctl start allserver-"+argv.replace("--", "")+".service", shell=True)
-            print(f"Systemd name -> allserver-"+argv.replace("--", "")+".service")
+            subprocess.run("systemctl daemon-reload && systemctl enable allserver-"+argv+".service && systemctl start allserver-"+argv+".service", shell=True)
+            print(f"Systemd name -> allserver-"+argv+".service")
         except:
             print(lang["Message"]["InstallAutoRun"]["Message"][3])
             return 2
     elif user_use_platform == "Windows":
         try:
-            file = open(f"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/allserver-"+argv.replace("--", "")+".bat", mode='w')
+            file = open(f"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/allserver-"+argv+".bat", mode='w')
             file.write(f"""cd {absolute_path}
 {start_command}
 pause""")
